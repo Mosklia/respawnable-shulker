1540f6f0159956f1f05c543f3f3627ae32681889
Mosklia <Sparky_14145@outlook.com>
2022-01-14 21:09:04 +0800

    Initial commit

--- src/main/java/cn/mosklia/respawnableshulker/ExampleMod.java
+++ src/main/java/cn/mosklia/respawnableshulker/ExampleMod.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package cn.mosklia.respawnableshulker;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
--- src/main/java/cn/mosklia/respawnableshulker/mixin/ExampleMixin.java
+++ src/main/java/cn/mosklia/respawnableshulker/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package cn.mosklia.respawnableshulker.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import cn.mosklia.respawnableshulker.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
